46a81db3ad1b16ad950bd975471f450b395c8bba
Akash Singh <aki24695@gmail.com>
2024-11-22 19:58:53 +0530

    add basic rl from candle

---
+++ rl_algorithms/src/main.rs
@@ -0,0 +1,32 @@
+/*
+export PYO3_PYTHON="$CONDA_PREFIX/bin/python"
+export PYTHON_SYS_EXECUTABLE="$CONDA_PREFIX/bin/python"
+export DYLD_LIBRARY_PATH="$CONDA_PREFIX/lib:$DYLD_LIBRARY_PATH"
+*/
+
+use candle::Result;
+use clap::{Parser, Subcommand};
+
+mod gym_env;
+mod vec_gym_env;
+
+mod policy_gradient;
+
+#[derive(Parser)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Pg,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    match args.command {
+        Command::Pg => policy_gradient::run()?,
+    }
+    Ok(())
+}
